20c899320b29a96fccbdfcf0ce9a2da4c4054d17
egbinta <egbintajerry@gmail.com>
2022-11-26 13:13:19 +0100

    files modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      date: "5th Feb 2023",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Doctors Appointment",
+      date: "5th Feb 2023",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Doctors Appointment",
+      date: "5th Feb 2023",
+      reminder: false,
+    },
+  ]);
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((tasks) => tasks.id !== id));
+  };
   return (
-    <div className="App">
-      <Header />
+    <div className="container">
+      <Header title="Task Tracker" />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
-const Header = () => {
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("clicked");
+  };
   return (
-    <header>
-      <h1>Task Tracker</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
+
+Header.defaultProps = {
+  title: "Task Tracker1",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// You can add css hear in js
+// const HeaderStyle = {
+//   color: "red",
+//   backgroundColor: "black",
+// };
 
 export default Header;
